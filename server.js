88f35bdf7a8cabe230d430c76182a4734b8377bf
amadeamargo <amadeamargo@gmail.com>
2022-03-18 02:43:12 -0500

    fixed get route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         err ? console.error("Could not retrive notes") : res.json(JSON.parse(data))
     })
